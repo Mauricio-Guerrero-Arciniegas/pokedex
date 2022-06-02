2015464b0acedd490a9c28894532be0625563605
Mauricio-Guerrero-Arciniegas <maurizioguerrero@hotmail.com>
2022-06-02 15:31:41 -0500

    pokedex3

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -1,8 +1,8 @@
-
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { ProgressBar } from 'react-bootstrap';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ProgressBar } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const PokemonDetail = () => {
   const [pokemon, setPokemon] = useState({});
@@ -16,7 +16,7 @@ const PokemonDetail = () => {
   }, [id]);
 
   return (
-    <div>
+    <div className="general">
       <img
         className="pokedeximg-2"
         src="https://i.pinimg.com/originals/bd/cd/20/bdcd20f5411ee5785889542d303ad4cb.png"
@@ -27,16 +27,19 @@ const PokemonDetail = () => {
       <div className="rectangle2-black"></div>
 
       <div className="detailcard">
+        <div className="gradient"></div>
+        <div className="detailimg">
         <img
           src={pokemon.sprites?.other["official-artwork"].front_default}
           alt=""
           className="pokemonimg"
         />
+        </div>
 
         <div className="initial">
           <h3>#{pokemon.id}</h3>
 
-          <h1>{pokemon.name}</h1>
+          <h1 className="name">{pokemon.name}</h1>
 
           <div className="weight-height">
             <div>
@@ -72,58 +75,55 @@ const PokemonDetail = () => {
 
           <div className="ability"></div>
 
-          <div className="stats">
-            <h3 className="progress">
-              <ProgressBar now={pokemon.stats?.[0].stat.name} />
-              {pokemon.stats?.[0].stat.name} : {pokemon.stats?.[0].base_stat}
+          <div className="stats">Stats
+            <h3>
+            {pokemon.stats?.[0].stat.name} :  <ProgressBar now={pokemon.stats?.[0].base_stat} />
             </h3>
             <h3>
-              {pokemon.stats?.[1].stat.name} : {pokemon.stats?.[1].base_stat}
+              {pokemon.stats?.[1].stat.name} : <ProgressBar now={pokemon.stats?.[1].base_stat} />
             </h3>
             <h3>
-              {pokemon.stats?.[2].stat.name} : {pokemon.stats?.[2].base_stat}
+              {pokemon.stats?.[2].stat.name} : <ProgressBar now={pokemon.stats?.[2].base_stat} />
             </h3>
 
             <h3>
-              {pokemon.stats?.[5].stat.name} : {pokemon.stats?.[5].base_stat}
+              {pokemon.stats?.[5].stat.name} : <ProgressBar now={pokemon.stats?.[5].base_stat} />
             </h3>
           </div>
-
+        </div>
       </div>
-        </div>
-        <div className="one">
-          <h3>{pokemon.moves?.[0]?.move.name}</h3>
-        </div>
-        <div className="two">
-          <h3>{pokemon.moves?.[1]?.move.name}</h3>
-        </div>
-        <div className="three">
-          <h3>{pokemon.moves?.[2].move.name}</h3>
-        </div>
-        <div className="four">
-          <h3>{pokemon.moves?.[3].move.name}</h3>
-        </div>
-        <div className="five">
-          <h3>{pokemon.moves?.[4].move.name}</h3>
-        </div>
-        <div className="six">
-          <h3>{pokemon.moves?.[5].move.name}</h3>
-        </div>
-        <div className="seven">
-          <h3>{pokemon.moves?.[6].move.name}</h3>
-        </div>
-        <div className="eight">
-          <h3>{pokemon.moves?.[7].move.name}</h3>
-        </div>
-        <div className="nine">
-          <h3>{pokemon.moves?.[8].move.name}</h3>
-        </div>
-        <div className="ten">
-          <h3>{pokemon.moves?.[9].move.name}</h3>
-        </div>
+      <div className="one">
+        <h3>{pokemon.moves?.[0]?.move.name}</h3>
+      </div>
+      <div className="two">
+        <h3>{pokemon.moves?.[1]?.move.name}</h3>
+      </div>
+      <div className="three">
+        <h3>{pokemon.moves?.[2].move.name}</h3>
+      </div>
+      <div className="four">
+        <h3>{pokemon.moves?.[3].move.name}</h3>
+      </div>
+      <div className="five">
+        <h3>{pokemon.moves?.[4].move.name}</h3>
+      </div>
+      <div className="six">
+        <h3>{pokemon.moves?.[5].move.name}</h3>
+      </div>
+      <div className="seven">
+        <h3>{pokemon.moves?.[6].move.name}</h3>
+      </div>
+      <div className="eight">
+        <h3>{pokemon.moves?.[7].move.name}</h3>
+      </div>
+      <div className="nine">
+        <h3>{pokemon.moves?.[8].move.name}</h3>
+      </div>
+      <div className="ten">
+        <h3>{pokemon.moves?.[9].move.name}</h3>
+      </div>
     </div>
   );
 };
 
 export default PokemonDetail;
-
